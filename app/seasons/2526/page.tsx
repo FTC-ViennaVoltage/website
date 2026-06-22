93038502e7fabed6623ae6b266e751f816549e11
pRicelessPanda <mitwannabe12@gmail.com>
2026-06-21 21:14:01 -0400

    Delete app/seasons/2526/page.tsx

--- app/seasons/2526/page.tsx
+++
@@ -1,5 +0,0 @@
-import SeasonDetail from "@/components/season-detail";
-
-export default function Season2526() {
-  return <SeasonDetail seasonName="2025–26" gameName="DECODE" seasonShort="2025–2026" />;
-}
